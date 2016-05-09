7a6ac6bc1639574093abf5f1ae7b2c4b84271f55
Titan <titan@kachebang.com>
2016-05-09 18:21:58 +0800

    Add more functions
    1. get
    2. list
    3. delete
    4. add_controller
    5. remove_controller

--- lib/api.ex
+++ lib/api.ex
@@ -23,6 +23,238 @@ defmodule Device.Api do
   @type uuid :: String.t
 
   @doc """
+  创建设备
+
+  ## 参数
+  |arg|type|meaning|
+  |---|----|-------|
+  |caller|Caller|调用者|
+  |did|uuid|设备 ID|
+  |data_stream_frequency|int|数据流更新频率|
+  |status_stream_frequency|int|状态流更新频率|
+  |configs|string=>string|设备配置|
+  |metrics|long|支持的采集类型集合|
+  |enabled|boolean|是否可用|
+
+  ## 结果
+
+  ### 成功
+
+  ```elixir
+  :ok
+  ```
+
+  ### 失败
+
+  ```elixir
+  {:error, code, reason}
+  ```
+
+  |code|reason|
+  |----|------|
+  |403|非法域访问|
+  |500|服务内部错误|
+
+  since: 0.1.0
+
+  """
+  @spec create(Caller.t, uuid, non_neg_integer, non_neg_integer, %{String.t => String.t}, non_neg_integer, boolean) :: :ok | {:error, code, reason}
+  def create(caller, did, data_stream_frequency, status_stream_frequency, configs, metrics, enabled \\ true) do
+    remote_call(:create, [caller, did, data_stream_frequency, status_stream_frequency, configs, metrics, enabled])
+  end
+
+  @doc """
+  删除设备
+
+  ## 参数
+  |arg|type|meaning|
+  |---|----|-------|
+  |caller|Caller|调用者|
+  |did|uuid|设备 ID|
+
+  ## 结果
+
+  ### 成功
+
+  ```elixir
+  :ok
+  ```
+
+  ### 失败
+
+  ```elixir
+  {:error, code, reason}
+  ```
+
+  |code|reason|
+  |----|------|
+  |403|非法域访问|
+  |404|设备不存在|
+  |500|服务内部错误|
+
+  since: 0.1.0
+
+  """
+  @spec delete(Caller.t, uuid) :: :ok | {:error, code, reason}
+  def delete(caller, did) do
+    remote_call(:delete, [caller, did])
+  end
+
+  @doc """
+  添加设备下面的控制器
+
+  ## 参数
+  |arg|type|meaning|
+  |---|----|-------|
+  |caller|Caller|调用者|
+  |did|uuid|设备 ID|
+  |no|integer|设备编号|
+
+  ## 结果
+
+  ### 成功
+
+  ```elixir
+  :ok
+  ```
+
+  ### 失败
+
+  ```elixir
+  {:error, code, reason}
+  ```
+
+  |code|reason|
+  |----|------|
+  |403|非法域访问|
+  |404|设备不存在|
+  |500|服务内部错误|
+
+  since: 0.1.0
+
+  """
+  @spec add_controller(Caller.t, uuid, non_neg_integer) :: :ok | {:error, code, reason}
+  def add_controller(caller, did, no) do
+    remote_call(:add_controller, [caller, did, no])
+  end
+
+  @doc """
+  删除设备下面的控制器
+
+  ## 参数
+  |arg|type|meaning|
+  |---|----|-------|
+  |caller|Caller|调用者|
+  |did|uuid|设备 ID|
+  |no|integer|设备编号|
+
+  ## 结果
+
+  ### 成功
+
+  ```elixir
+  :ok
+  ```
+
+  ### 失败
+
+  ```elixir
+  {:error, code, reason}
+  ```
+
+  |code|reason|
+  |----|------|
+  |403|非法域访问|
+  |404|设备不存在|
+  |500|服务内部错误|
+
+  since: 0.1.0
+
+  """
+  @spec remove_controller(Caller.t, uuid, non_neg_integer) :: :ok | {:error, code, reason}
+  def remove_controller(caller, did, no) do
+    remote_call(:remove_controller, [caller, did, no])
+  end
+
+  @doc """
+  获得设备列表。
+
+  ## 参数
+
+  |arg|type|meaning|
+  |---|----|-------|
+  |caller|Caller|调用者|
+  |enabled|boolean|列出有效的或无效的设备|
+  |offset|integer|结果集游标偏离量|
+  |limit|integer|结果集大小|
+
+  ## 结果
+
+  ### 成功
+
+  ```elixir
+  {:ok, [Entity.t]}
+  ```
+
+  ### 失败
+
+  ```elixir
+  {:error, code, reason}
+  ```
+
+  |code|reason|
+  |----|------|
+  |403|非法域访问|
+  |404|设备不存在|
+  |500|服务内部错误|
+
+  since: 0.1.0
+
+  """
+  @spec list(Caller.t, boolean, non_neg_integer, non_neg_integer) :: {:ok, [Entity.t]} | {:error, code, reason}
+  def list(caller, enabled, offset, limit \\ 20) do
+    remote_call(:list, [caller, enabled, offset, limit])
+  end
+
+  @doc """
+  获得设备详情。
+
+  ## 参数
+
+  |arg|type|meaning|
+  |---|----|-------|
+  |caller|Caller|调用者|
+  |did|uuid|设备 ID|
+
+  ## 结果
+
+  ### 成功
+
+  ```elixir
+  {:ok, Entity.t}
+  ```
+
+  ### 失败
+
+  ```elixir
+  {:error, code, reason}
+  ```
+
+  |code|reason|
+  |----|------|
+  |403|非法域访问|
+  |404|设备不存在|
+  |500|服务内部错误|
+
+  since: 0.1.0
+
+  """
+  @spec get(Caller.t, uuid) :: {:ok, Entity.t} | {:error, code, reason}
+  def get(caller, did) do
+    remote_call(:get, [caller, did])
+  end
+
+  @doc """
   启用设备。在修改设备状态的同时，更新设备信息到 Service
 
   ## 参数
--- lib/entity.ex
+++ lib/entity.ex
@@ -1,4 +1,4 @@
 defmodule Device.Entity do
-  defstruct did: nil, name: nil, fireware: nil, config: nil, enabled: true
-  @type t :: %Device.Entity{did: String.t, name: String.t, fireware: String.t, config: String.t, enabled: boolean}
+  defstruct did: nil, name: nil, fireware: nil, config: nil, enabled: true, controllers: []
+  @type t :: %Device.Entity{did: String.t, name: String.t, fireware: String.t, config: String.t, enabled: boolean, controllers: [non_neg_integer]}
 end
