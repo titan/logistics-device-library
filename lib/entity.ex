d1e35616b644bc7d91078ad181a8467a400e3731
Titan <titan@kachebang.com>
2016-05-10 11:50:53 +0800

    Add metrics to entity

--- lib/entity.ex
+++ lib/entity.ex
@@ -1,4 +1,4 @@
 defmodule Device.Entity do
-  defstruct did: nil, name: nil, data_stream_frequency: 0, status_stream_frequency: 0, configs: nil, enabled: true, controllers: []
-  @type t :: %Device.Entity{did: String.t, name: String.t, data_stream_frequency: non_neg_integer, status_stream_frequency: non_neg_integer, configs: %{String.t => String.t}, enabled: boolean, controllers: [non_neg_integer]}
+  defstruct did: nil, name: nil, data_stream_frequency: 0, status_stream_frequency: 0, configs: nil, metrics: 0, enabled: true, controllers: []
+  @type t :: %Device.Entity{did: String.t, name: String.t, data_stream_frequency: non_neg_integer, status_stream_frequency: non_neg_integer, configs: %{String.t => String.t}, metrics: non_neg_integer, enabled: boolean, controllers: [non_neg_integer]}
 end
