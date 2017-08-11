61d57cba1ec6d84d9522bc6e94beb9623b360ed8
Titan <titan@kachebang.com>
2017-08-11 20:51:33 +0800

    Change repository of domain-library

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule Device.Library.Mixfile do
   defp deps do
     [
       {:resource_discovery, git: "https://github.com/titan/resource_discovery.git", branch: "master"},
-      {:domainlib, git: "git@gitlab.ruicloud.cn:titan/domainlib.git", tag: "0.1.0" },
+      {:domainlib, git: "git@github.com:titan/logistics-domain-library.git", tag: "0.1.0" },
       {:ex_doc, "~> 0.11", only: :dev}
     ]
   end
