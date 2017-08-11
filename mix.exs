e0fb5fe935c24244fe7a8bbb5d5d8009b36ed298
Titan <titan@kachebang.com>
2017-08-11 20:54:31 +0800

    Refactor repository of domain-library

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule Device.Library.Mixfile do
   defp deps do
     [
       {:resource_discovery, git: "https://github.com/titan/resource_discovery.git", branch: "master"},
-      {:domainlib, git: "git@github.com:titan/logistics-domain-library.git", tag: "0.1.0" },
+      {:domainlib, git: "git@github.com:titan/logistics-domain-library.git", branch: "master" },
       {:ex_doc, "~> 0.11", only: :dev}
     ]
   end
